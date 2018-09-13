e10be90435f4eb618021ef397563930f5f7dfd32
simont1 <stsui2@stuy.edu>
2018-09-12 22:08:21 -0400

    added 02_randomizer

---
+++ 02_randomizer/Gudetama_SongD-TsuiS.py
@@ -0,0 +1,23 @@
+# Derek Song and Simon Tsui
+# Soft Dev - PD8
+# K #02: NO-body expects the Spanish Inquisition!
+# 2018 - 9 - 7
+
+import random
+
+KREWES = {
+
+        'w': ['William Lu', 'Qian', 'Peter', 'Ahnaf', 'Kenny', 'Sophia', 'Sajed', 'Emily', 'Hasif', 'Brian ', 'Dennis', 'Jiayang', 'Shafali ', 'Isaac ', 'Tania', 'Derek', 'Shin', 'Vincent', 'Ricky', 'Puneet', 'Wei Wen', 'Tim', 'Jeffrey', 'Joyce ', 'Mohtasim', 'Simon', 'Thomas', 'Ray', 'Jack', 'Karen', 'Robin', 'Jabir', 'Johnny ', 'Matthew', 'Johnson Li', 'Angela', 'Crystal', 'Jiajie', 'Theodore (Dont really care)', 'Anton', 'Max', 'Bo', 'Andrew', 'Kendrick', 'Kevin', 'Kyle', 'Jamil', 'Mohammed', 'Ryan', 'Jason'],
+
+        'm': ['Daniel', 'Aleksandra', 'Addison', 'Hui Min', 'Aaron', 'Rubin', 'Raunak', 'Stefan', 'Cheryl', 'Cathy', 'Mai', 'Claire ', 'Alex', 'Bill', 'Daniel', 'Jason'],
+
+        'x': ['Derek', 'Britni', 'Joan', 'Vincent', 'Jared', 'Ivan', 'Thomas', 'Maggie', 'Damian', 'Tina', 'Fabiha', 'John', 'Susan ', 'Kaitlin', 'Michelle', 'Clara', 'Rachel', 'Amit', 'Jerry', 'Raymond', 'Zane', 'Soojin', 'Maryann', 'Adil', 'Josh', 'Imad']
+
+}
+
+def randomizer():
+    for i in KREWES: # goes through each key
+        index = random.randrange(len(KREWES[i])) # determines a random number from 0 to the length of the key - 1
+        print(KREWES[i][index]) # prints a random name from the key
+        
+randomizer()
