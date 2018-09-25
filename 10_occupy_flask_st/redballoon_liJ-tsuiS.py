d4d9d4d2eb86b820bdfd56f480a2b0a5adebc71f
Simon <stsui2@stuy.edu>
2018-09-24 21:02:49 -0400

    post evaluation

--- 10_occupy_flask_st/redballoon_liJ-tsuiS.py
+++ 10_occupy_flask_st/redballoon_liJ-tsuiS.py
@@ -12,31 +12,31 @@ def convert(filename):
     #open file and parse it into a generator using csv reader
     #convert the generator into a list exluding the job and percentage table headers and footers
     #read values two at a time as tuples and use those to create a key value pair
-    f = {k:float(v) for k,v in list(csv.reader(open(filename)))[1:-1]}
-    return f
+    dict = {key:float(value) for key,value in list(csv.reader(open(filename)))[1:-1]}
+    return dict
 
 def pickRandom(f):
     '''takes a dictionary of key value pairs and returns a random key using the values as the percent chance of selecting the corresponding key'''
     #pick a number, any number (as long as it's an element of [0,99.8))
     rand = random.uniform(0, 99.8)
-    #go through key value pairs in f
-    for k,v in f.items():
-        rand -= v
+    #go through key value pairs in dict
+    for key,value in dict.items():
+        rand -= value
         #when the cumulative total is greater than the random value, return that key
         if rand <= 0:
-            return k
+            return key
     return "unemployed"
 
 def randOcc():
-    f = convert("occupations.csv")
+    dict = convert("occupations.csv")
     return "weighted random pick: " + pickRandom(f)
 
 app = Flask(__name__)
 
 @app.route("/occupations")
 def occupations():
-    f = convert("occupations.csv")
-    return render_template("template.html", randomOcc = randOcc(), d=f)
+    dict = convert("occupations.csv")
+    return render_template("template.html", randomOcc = randOcc(), d=dict)
 
 app.debug = True
 app.run()
